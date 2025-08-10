42a8083098e43cb26a819f9343c8e4b7b4358c57
Divyansh jamwal <divyanshxanshu@gmail.com>
2025-08-10 21:16:30 +0530

    removes 2 callbacks clashing the server

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const pgSession = require("connect-pg-simple")(session);
 const path = require("path");
 require("dotenv").config();
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const http = require("http");
 const { Server } = require("socket.io");
 const db = require("./db");
@@ -35,68 +34,6 @@ app.use(
 // Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
-
-// ========== GOOGLE OAUTH STRATEGY ==========
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const email = profile.emails[0].value;
-      const name = profile.displayName;
-
-      // Restrict to college domain
-      if (!email.endsWith("@nith.ac.in")) {
-        return done(null, false, { message: "Only @nith.ac.in emails allowed" });
-      }
-
-      try {
-        const existingUser = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-        if (existingUser.rows.length > 0) {
-          return done(null, existingUser.rows[0]);
-        }
-
-        const newUser = await db.query(
-          "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
-          [name, email, "0"]
-        );
-        return done(null, newUser.rows[0]);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
-    done(null, result.rows[0]);
-  } catch (err) {
-    done(err);
-  }
-});
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    req.session.user = {
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email,
-      photo: req.user.photo || req.user.photos?.[0]?.value || null
-    };
-    res.redirect("/menu");
-  }
-);
 
 // ======== MENU ROUTE ========
 app.get("/menu", ensureAuth, async (req, res) => {
@@ -222,6 +159,7 @@ io.on("connection", (socket) => {
     console.log("User disconnected: " + socket.id);
   });
 });
+
 // Use routers
 app.use("/", authRouter);
 app.use("/books", booksRouter);
