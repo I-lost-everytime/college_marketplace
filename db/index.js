f395b3d87af6ebd507507493f13704c0d18e4ed1
Divyansh jamwal <divyanshxanshu@gmail.com>
2025-08-31 12:44:42 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? {
-    rejectUnauthorized: false
-  } : false
+  ssl: {
+    rejectUnauthorized: false, // Required for Render
+  },
 });
 
 module.exports = pool;
